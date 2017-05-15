f1cb069453828eee4d571753d96f3c8bb587791d
Jakob Schaal <jakob.schaal@hotmail.com>
2017-05-15 13:49:08 +0200

    Added a general include file "BrotBoxEngine.h"

---
+++ BrotBoxEngine/BrotBoxEngine.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Array.h";
+#include "DynamicArray.h"
+#include "List.h"
+
+#include "String.h"
+
+#include "DefaultDestroyer.h"
+#include "GeneralPurposeAllocator.h"
+#include "PoolAllocator.h"
+#include "StackAllocator.h"
+#include "STLAllocator.h"
+#include "UniquePointer.h"
+
+#include "CPUWatch.h"
+#include "StopWatch.h"
+
+#include "DataType.h"
+#include "STLCapsule.h"
+#include "UtilDebug.h"
+#include "UtilMath.h"
