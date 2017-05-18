9b4d412586a7f181317b7841a62f1c2dc0fdcb4b
Jakob Schaal <jakob.schaal@hotmail.com>
2017-05-18 18:31:49 +0200

    Added abs function to UtilMath.h

--- BrotBoxEngine/UtilMath.h
+++ BrotBoxEngine/UtilMath.h
@@ -4,8 +4,19 @@
 namespace bbe
 {
 	template<typename T>
-	constexpr T nextMultiple(T multipleOf, T value)
+	constexpr T nextMultiple(const T &multipleOf, const T &value)
 	{
 		return (value + multipleOf - 1) - ((value + multipleOf - 1) % multipleOf);
+	}
+
+	template<typename T>
+	constexpr T abs(const T &val)
+	{
+		//UNTESTED
+		if (val < 0) {
+			return -val;
+		}
+
+		return val;
 	}
 }
