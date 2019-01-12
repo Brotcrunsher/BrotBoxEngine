35b266d34fba16253875c97d6e3f56f266ddae7c
Jakob Schaal <jakob.schaal@hotmail.com>
2019-01-12 17:33:06 +0100

    UNSTABLE COMMIT! Removed wchar_t usage in simpleFile

--- BrotBoxEngine/SimpleFile.cpp
+++ BrotBoxEngine/SimpleFile.cpp
@@ -7,35 +7,7 @@
 bbe::List<char> bbe::simpleFile::readBinaryFile(const bbe::String & filepath)
 {
 	//UNTESTED
-	std::cout << filepath.getLength() << std::endl;
-	std::cout << filepath.getRaw() << std::endl;
-	for(std::size_t i = 0; i<filepath.getLength(); i++){
-		std::cout << filepath.getRaw()[i] << std::endl;
-	}
-
-	wchar_t path_w[1024];
-	char path[1024];	
-	memset(path_w, 0, 1024*sizeof(wchar_t));
-
-	std::cout << "OLA!" << std::endl;
-	if(filepath.getLength() >= sizeof(path) - 1)
-	{
-		throw std::runtime_error("Path too long!");
-	}
-	for(std::size_t i = 0; i<filepath.getLength(); i++)
-	{
-		std::cout << "OLA!" << std::endl;
-		path_w[i] = filepath.getRaw()[i];
-	}
-
-	std::cout << "OLA!" << std::endl;
-	std::size_t length = wcstombs(path, path_w, sizeof(path));
-	if(length >= sizeof(path) - 1)
-	{
-		throw std::runtime_error("Path too long!");
-	}
-	std::cout << "Reading File: " << path << std::endl;
-	std::ifstream file(path, std::ios::binary | std::ios::ate);
+	std::ifstream file(filepath.getRaw(), std::ios::binary | std::ios::ate);
 
 	if (file)
 	{
@@ -55,13 +27,7 @@ bbe::List<char> bbe::simpleFile::readBinaryFile(const bbe::String & filepath)
 
 void bbe::simpleFile::writeFloatArrToFile(const bbe::String & filePath, float * arr, size_t size)
 {	
-	char path[1024];
-	std::size_t length = wcstombs(path, filePath.getRaw(), sizeof(path));
-	if(length >= sizeof(path) - 1)
-	{
-		throw std::runtime_error("Path too long!");
-	}
-	std::ofstream file(path);
+	std::ofstream file(filePath.getRaw());
 	for (std::size_t i = 0; i < size; i++)
 	{
 		file << arr[i] << "\n";
