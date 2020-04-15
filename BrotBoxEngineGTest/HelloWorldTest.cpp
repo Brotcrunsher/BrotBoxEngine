1d2709735dc02f43f633c692fe5737b212f4544d
Jakob Schaal <jakob.schaal@hotmail.com>
2020-04-15 12:56:43 +0200

    Added GTest Template

---
+++ BrotBoxEngineGTest/HelloWorldTest.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+#include <iostream>
+
+TEST(HelloWorld, HelloWorld) {
+	std::cout << "Oh hai!" << std::endl;
+}
---
+++ BrotBoxEngineGTest/MainGTest.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+
+int main(int argc, char **argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::GTEST_FLAG(filter) = "**";
+	
+	return RUN_ALL_TESTS();
+}
