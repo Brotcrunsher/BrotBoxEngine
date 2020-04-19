58712a49ea85a5f28b395cd4486c8b9a28c17b28
Jakob <brotcrunsher@hotmail.de>
2020-04-19 12:20:26 +0200

    Refactored ExampleBalloonGame

--- ExampleBalloonGame/ExampleBalloonGame.cpp
+++ ExampleBalloonGame/ExampleBalloonGame.cpp
@@ -4,125 +4,111 @@
 constexpr int WINDOW_WIDTH = 1920;
 constexpr int WINDOW_HEIGHT = 1080;
 
-
 class MyGame : public bbe::Game
 {
-
+private:
 	class Balloon {
-
 	public:
-		int x;
-		int y;
-		int heigth;
-		int width;
+		float x;
+		float y;
+		float width;
+		float heigth;
 		float hue;
 
-		Balloon(int x, int y, int width, int heigth, float hue) {
-			this->x = x;
-			this->y = y;
-
-			this->width = width;
-			this->heigth = heigth;
-			this->hue = hue;
+		Balloon(float x, float y, float width, float heigth, float hue)
+			: x(x), y(y), width(width), heigth(heigth), hue(hue)
+		{
 		}
-
 	};
 
+	static constexpr int BALLOON_WIDTH = 40;
+	static constexpr int BALLOON_HEIGTH = 60;
 
-	int BALLOON_WIDTH = 40;
-	int BALLOON_HEIGTH = 60;
-	int SCORE = 0;
-	bbe::Font myFont;
-	bbe::Random random;
 	bool gameover = false;
+	int score = 0;
+
+	bbe::Font font;
+	bbe::Random random;
 	bbe::Image background;
-
 	bbe::List<Balloon> balloons;
-
-	virtual void onStart() override
-	{
-		myFont.load("arial.ttf", 200);
-		background.load("sky.jpg");
-		addBalloon();
-
-	}
 
 	void addBalloon() {
 		balloons.add(Balloon(random.randomInt(WINDOW_WIDTH - BALLOON_WIDTH), WINDOW_HEIGHT,
-			 BALLOON_WIDTH, BALLOON_HEIGTH, randomBalloonHue()));
-	}
-
-	bbe::Color randomBalloonColor() {
-		return bbe::Color(random.randomFloat(), random.randomFloat(), 0);
+			BALLOON_WIDTH, BALLOON_HEIGTH, randomBalloonHue()));
 	}
 
 	float randomBalloonHue() {
-		return random.randomFloat()*360;
+		return random.randomFloat() * 360;
+	}
+
+	bool checkGameOver(const Balloon& b) {
+		return (b.y <= font.getFontSize() + 40);
+	}
+
+	bool checkMousePosition(const Balloon& b) {
+		return (getMouseY() < b.y + b.heigth && getMouseY() > b.y)
+			&& (getMouseX() < b.x + b.width  && getMouseX() > b.x);
+	}
+
+public:
+	virtual void onStart() override
+	{
+		font.load("arial.ttf", 200);
+		background.load("sky.jpg");
+		addBalloon();
 	}
 
 	virtual void update(float timeSinceLastFrame) override
 	{
-
 		if (gameover) return;
 
 		for (Balloon& b : balloons) {
-			
 			if (checkGameOver(b)) {
 				gameover = true;
 			}
-			// check game over
+
 			if (checkMousePosition(b) && isMousePressed(bbe::MouseButton::LEFT)) {
 				b.y = WINDOW_HEIGHT;
-				b.x = random.randomInt(WINDOW_WIDTH - b.width);
+				b.x = random.randomFloat(WINDOW_WIDTH - b.width);
 				b.hue = randomBalloonHue();
-				SCORE++;
+				score++;
 
 				if (random.randomFloat()<0.2) {
 					addBalloon();
 				}
 			}
-			b.y--;
+			b.y-=timeSinceLastFrame*60;
 		}
-	}
-
-	bool checkGameOver(const Balloon& b) {
-		return  (b.y <= myFont.getFontSize() + 40);
-	}
-
-	bool checkMousePosition(const Balloon &b) {
-		return (getMouseY() < b.y + b.heigth  && getMouseY() > b.y )
-			&& (getMouseX() < b.x + b.width && getMouseX() > b.x);
 	}
 
 	virtual void draw3D(bbe::PrimitiveBrush3D & brush) override
 	{
 	}
+
 	virtual void draw2D(bbe::PrimitiveBrush2D& brush) override
 	{
-		
-		brush.setColorRGB(bbe::Color(1, 1, 1));
-		brush.fillRect(bbe::Rectangle(0, 0, WINDOW_WIDTH, myFont.getFontSize() + 40));
+		brush.setColorRGB(1, 1, 1);
+		brush.fillRect(bbe::Rectangle(0, 0, WINDOW_WIDTH, font.getFontSize() + 40));
 
-		brush.setColorRGB(bbe::Color(0, 0, 0));
+		brush.setColorRGB(0, 0, 0);
 		bbe::String currentScore = "Your Score: ";
-		currentScore += SCORE;
-		brush.fillText(10, myFont.getFontSize() + 20, currentScore.getRaw(), myFont);
+		currentScore += score;
+		brush.fillText(10, font.getFontSize() + 20, currentScore.getRaw(), font);
 
-		brush.setColorRGB(bbe::Color(0, 0, 0));
-		brush.fillLine(0, myFont.getFontSize() + 40, WINDOW_WIDTH, myFont.getFontSize() + 40, 5);
+		brush.setColorRGB(0, 0, 0);
+		brush.fillLine(0, font.getFontSize() + 40, WINDOW_WIDTH, font.getFontSize() + 40, 5);
 
+		brush.setColorRGB(1, 1, 1);
+		brush.drawImage(bbe::Rectangle(0, font.getFontSize() + 40, WINDOW_WIDTH, background.getHeight()), background);
 
-		brush.drawImage(bbe::Rectangle(0, myFont.getFontSize() + 40, WINDOW_WIDTH, WINDOW_HEIGHT), background);
-
-		// for each balloon draw
 		for (Balloon& b : balloons) {
 			brush.setColorHSV(b.hue, 1, 1);
 			brush.fillCircle(bbe::Circle(b.x, b.y, b.width, b.heigth));
 		}
 		
 		if (gameover){
-			brush.setColorRGB(bbe::Color(1, 0, 0));
-			brush.fillText(10, WINDOW_HEIGHT/2, "Game Over!", myFont);
+			brush.setColorRGB(1, 0, 0);
+			brush.fillText(10, WINDOW_HEIGHT/2, "Game Over!", font);
 
 		}
 	}
@@ -130,9 +116,6 @@ class MyGame : public bbe::Game
 	virtual void onEnd() override
 	{
 	}
-
-
-	
 };
 
 int main()
