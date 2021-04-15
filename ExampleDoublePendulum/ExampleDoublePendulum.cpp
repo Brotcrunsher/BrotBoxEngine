93fbbe6d4411353e6a38ce450f1d950a7111af13
Jakob Schaal <jakob.schaal@hotmail.com>
2021-04-15 21:29:33 +0200

    Made Double Pendulum more fancy. Different states, resetting etc.

--- ExampleDoublePendulum/ExampleDoublePendulum.cpp
+++ ExampleDoublePendulum/ExampleDoublePendulum.cpp
@@ -5,8 +5,40 @@
 constexpr int WINDOW_WIDTH = 1280;
 constexpr int WINDOW_HEIGHT = 720;
 
+enum class State
+{
+	physics,
+	slow_down,
+	freeze,
+	restarting,
+};
+
+State& operator++(State& s)
+{
+	switch (s)
+	{
+	case State::physics:    return s = State::slow_down;
+	case State::slow_down:  return s = State::freeze;
+	case State::freeze:     return s = State::restarting;
+	case State::restarting: return s = State::physics;
+	}
+}
+
+float getStateDuration(State& s)
+{
+	switch (s)
+	{
+	case State::physics:    return 60.f;
+	case State::slow_down:  return 1.f;
+	case State::freeze:		return 1.f;
+	case State::restarting: return 3.f;
+	}
+}
+
 class MyGame : public bbe::Game
 {
+	State state = State::freeze;
+
 	class DoublePendulum
 	{
 	public:
@@ -23,6 +55,9 @@ class MyGame : public bbe::Game
 		constexpr static double m1 = 10;
 		constexpr static double m2 = 1;
 		constexpr static double tickTime = 0.0001;
+
+		double interpolationStartOmega1;
+		double interpolationStartOmega2;
 
 		double omega1;
 		double omega2;
@@ -80,9 +115,10 @@ class MyGame : public bbe::Game
 	{
 		for (int i = 0; i < 5000; i++)
 		{
+			float percentage = (float)i / 5000.f;
 			pendulums.add(DoublePendulum(
-				bbe::Math::PI / 2 + 0.0000001 * i,
-				bbe::Math::PI / 2,
+				bbe::Math::TAU * percentage,
+				bbe::Math::TAU * percentage,
 				-bbe::Math::PI / 2,
 				-bbe::Math::PI / 2
 			));
@@ -102,23 +138,73 @@ class MyGame : public bbe::Game
 	
 	virtual void update(float timeSinceLastFrame) override
 	{
-		static float timeSinceLastTick = 0;
-		timeSinceLastTick += timeSinceLastFrame;
+		static float stateTime = 0;
+		stateTime += timeSinceLastFrame;
+		float statePercentage = stateTime / getStateDuration(state);
+		if (statePercentage > 1) statePercentage = 1;
 
-		size_t ticks = timeSinceLastTick / DoublePendulum::tickTime;
-		if (ticks > 0)
+		if (state == State::physics
+			|| state == State::slow_down)
 		{
-			timeSinceLastTick -= ticks * DoublePendulum::tickTime; timeSinceLastTick -= DoublePendulum::tickTime;
+			static float timeSinceLastTick = 0;
+			timeSinceLastTick += timeSinceLastFrame;
 
-			bbe::List<std::future<void>> futures;
-			const size_t increment = pendulums.getLength() / 12;
-			for (size_t i = 0; i < pendulums.getLength(); i += increment)
+			size_t ticks = timeSinceLastTick / DoublePendulum::tickTime;
+			if (ticks > 0)
 			{
-				futures.add(std::async(std::launch::async, &MyGame::tickPendulums, this, i, i + increment, ticks));
+				timeSinceLastTick -= ticks * DoublePendulum::tickTime; timeSinceLastTick -= DoublePendulum::tickTime;
+
+				if (state == State::slow_down)
+				{
+					ticks = (size_t)((float)ticks * (1.f - statePercentage));
+				}
+
+				bbe::List<std::future<void>> futures;
+				const size_t increment = pendulums.getLength() / 12;
+				for (size_t i = 0; i < pendulums.getLength(); i += increment)
+				{
+					futures.add(std::async(std::launch::async, &MyGame::tickPendulums, this, i, i + increment, ticks));
+				}
+				for (size_t i = 0; i < futures.getLength(); i++)
+				{
+					futures[i].wait();
+				}
 			}
-			for (size_t i = 0; i < futures.getLength(); i++)
+		}
+		else if (state == State::restarting)
+		{
+			for (size_t i = 0; i < pendulums.getLength(); i++)
 			{
-				futures[i].wait();
+				pendulums[i].omega1 = bbe::Math::interpolateCosine(pendulums[i].interpolationStartOmega1, pendulums[0].omega1, statePercentage);
+				pendulums[i].omega2 = bbe::Math::interpolateCosine(pendulums[i].interpolationStartOmega2, pendulums[0].omega2, statePercentage);
+			}
+		}
+
+		if (statePercentage == 1)
+		{
+			++state;
+			stateTime = 0;
+
+			if (state == State::physics)
+			{
+				for (size_t i = 0; i < pendulums.getLength(); i++)
+				{
+					pendulums[i].omega1 = pendulums[0].omega1 + 0.0000001 * i;
+					pendulums[i].omega2 = pendulums[0].omega2;
+					pendulums[i].velo1 = pendulums[0].velo1;
+					pendulums[i].velo2 = pendulums[0].velo2;
+				}
+			}
+
+			if (state == State::restarting)
+			{
+				for (size_t i = 0; i < pendulums.getLength(); i++)
+				{
+					pendulums[i].interpolationStartOmega1 = pendulums[i].omega1;
+					pendulums[i].interpolationStartOmega2 = pendulums[i].omega2;
+					pendulums[i].velo1 = pendulums[0].velo1;
+					pendulums[i].velo2 = pendulums[0].velo2;
+				}
 			}
 		}
 	}
@@ -134,7 +220,7 @@ class MyGame : public bbe::Game
 		}
 		for (size_t k = 0; k < pendulums.getLength(); k++)
 		{
-			brush.setColorHSV(360.f * k / pendulums.getLength(), 1, 1);
+			brush.setColorHSV(360.f * k / pendulums.getLength(), 1, 1, 0.1f);
 			pendulums[k].draw(brush, DoublePendulum::DrawMode::OUTER);
 		}
 	}
@@ -146,6 +232,9 @@ class MyGame : public bbe::Game
 int main()
 {
 	MyGame *mg = new MyGame();
+	// Enable these to record. WARNING: Generates a LOT of images!
+	//mg->setScreenshotRecordingMode();
+	//mg->setMaxFrame(10 * 60 * 60);
 	mg->start(WINDOW_WIDTH, WINDOW_HEIGHT, "Double Pendulum!");
 	delete mg;
 }
