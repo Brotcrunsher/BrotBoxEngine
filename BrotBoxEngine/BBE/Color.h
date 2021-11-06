b6dddad28b37117d7206b16d42a0868dee2c7e4e
Brotcrunsher <brotcrunsher@hotmail.de>
2021-11-06 14:04:12 +0100

    Constexpr constructors for Color.

--- BrotBoxEngine/BBE/Color.h
+++ BrotBoxEngine/BBE/Color.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "../BBE/Vector3.h"
+#include "../BBE/String.h"
 
 namespace bbe
 {
@@ -11,15 +12,26 @@ namespace bbe
 		float b;
 		float a;
 
-		Color();
-		Color(float r, float g, float b);
-		Color(float r, float g, float b, float a);
+		constexpr Color()
+			: r(0), g(0), b(0), a(1)
+		{}
+		constexpr Color(float r, float g, float b)
+			: r(r), g(g), b(b), a(1)
+		{}
+		constexpr Color(float r, float g, float b, float a)
+			: r(r), g(g), b(b), a(a)
+		{}
 
 		bool operator== (const bbe::Color& other) const;
 		bool operator!= (const bbe::Color& other) const;
 
 		bbe::Color operator* (float scalar) const;
 		bbe::Color operator/ (float scalar) const;
+
+		bbe::Color& operator*= (float scalar);
+		bbe::Color& operator/= (float scalar);
+
+		bbe::String toHex() const;
 
 		static Vector3 HSVtoRGB(float h, float s, float v);
 	};
