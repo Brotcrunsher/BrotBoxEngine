d6726d8e3b4063bd564a4920117ed12d1bc342c6
Brotcrunsher <Brotcrunsher@hotmail.de>
2026-03-10 20:41:00 +0100

    Upgrading gtest.

--- Tests/BrotBoxEngineGTest/google_test_subdir.cmake
+++ Tests/BrotBoxEngineGTest/google_test_subdir.cmake
@@ -35,7 +35,7 @@ project(googletest-download NONE)
 include(ExternalProject)
 ExternalProject_Add(googletest
   GIT_REPOSITORY    https://github.com/google/googletest.git
-  GIT_TAG           v1.15.2
+  GIT_TAG           v1.17.0
   SOURCE_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-src"
   BINARY_DIR        "${CMAKE_CURRENT_BINARY_DIR}/googletest-build"
   CONFIGURE_COMMAND ""
