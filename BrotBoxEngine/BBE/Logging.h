f29e3659d16945d1738b688e8b417c281fbe2b6c
Brotcrunsher <brotcrunsher@hotmail.de>
2026-04-02 17:43:13 +0200

    Logging times.

--- BrotBoxEngine/BBE/Logging.h
+++ BrotBoxEngine/BBE/Logging.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <sstream>
+#include <string>
 #include "../BBE/List.h"
 #include "../BBE/String.h"
 
@@ -13,26 +14,29 @@ namespace bbe
 		{
 			void partialLog(const char *c);
 			void fullLog(const char *c);
+			std::string currentTimestamp();
 		}
 
 		const bbe::ConcurrentList<bbe::String> &getLog();
 	}
 }
 
-#define BBELOG(out)                                               \
-	do                                                            \
-	{                                                             \
-		std::stringstream stream;                                 \
-		stream << out;                                            \
-		std::cout << stream.str();                                \
-		bbe::logging::INTERNAL::partialLog(stream.str().c_str()); \
+#define BBELOG(out)                                                          \
+	do                                                                      \
+	{                                                                       \
+		std::string bbelog_ts_ = bbe::logging::INTERNAL::currentTimestamp(); \
+		std::stringstream stream;                                           \
+		stream << bbelog_ts_ << out;                                        \
+		std::cout << stream.str();                                          \
+		bbe::logging::INTERNAL::partialLog(stream.str().c_str());           \
 	} while (false)
 
-#define BBELOGLN(out)                                          \
-	do                                                         \
-	{                                                          \
-		std::stringstream stream;                              \
-		stream << out;                                         \
-		std::cout << stream.str() << std::endl;                \
-		bbe::logging::INTERNAL::fullLog(stream.str().c_str()); \
+#define BBELOGLN(out)                                                       \
+	do                                                                      \
+	{                                                                       \
+		std::string bbelog_ts_ = bbe::logging::INTERNAL::currentTimestamp(); \
+		std::stringstream stream;                                           \
+		stream << bbelog_ts_ << out;                                        \
+		std::cout << stream.str() << std::endl;                             \
+		bbe::logging::INTERNAL::fullLog(stream.str().c_str());              \
 	} while (false)
--- BrotBoxEngine/Logging.cpp
+++ BrotBoxEngine/Logging.cpp
@@ -1,4 +1,7 @@
 #include "BBE/Logging.h"
+#include <chrono>
+#include <ctime>
+#include <cstdio>
 
 static bbe::ConcurrentList<bbe::String> logLines = { "" };
 
@@ -12,6 +15,18 @@ void bbe::logging::INTERNAL::fullLog(const char *c)
 {
 	partialLog(c);
 	logLines.add("");
+}
+
+std::string bbe::logging::INTERNAL::currentTimestamp()
+{
+	auto now = std::chrono::system_clock::now();
+	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+	std::time_t t = std::chrono::system_clock::to_time_t(now);
+	std::tm *tm = std::localtime(&t);
+	char buf[16];
+	std::snprintf(buf, sizeof(buf), "[%02d:%02d:%02d:%03d] ",
+		tm->tm_hour, tm->tm_min, tm->tm_sec, static_cast<int>(ms.count()));
+	return buf;
 }
 
 const bbe::ConcurrentList<bbe::String> &bbe::logging::getLog()
